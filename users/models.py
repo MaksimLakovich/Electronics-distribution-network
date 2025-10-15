6130b5d62b7bc394bb4396ce07b81d21064e5b69
Maksim Lakovich <maks.lakovich@gmail.com>
2025-10-15 14:11:35 +0300

    Создана модель AppUser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.managers import AppUserManager
+
+
+class AppUser(AbstractUser):
+    """Модель представляет пользователя на платформе (авторизация по email)."""
+
+    username = None  # type: ignore
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Почта (username):",
+        help_text="Введите email",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    # Указываю кастомный менеджер для пользователя без поля username.
+    # Указываю "type: ignore" чтоб убрать ошибку типизации mypy, так как пока не разобрался как правильно
+    # работать с типизацией TypeVar.
+    objects = AppUserManager()  # type: ignore
+
+    def __str__(self):
+        """Метод определяет строковое представление объекта. Полезно для отображения объектов в админке/консоли."""
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["email"]
