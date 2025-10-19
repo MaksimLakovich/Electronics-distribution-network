e9716e319b4cb551ca9d239881ccb9a6a266cf85
Maksim Lakovich <maks.lakovich@gmail.com>
2025-10-20 01:53:17 +0300

    Подключена API-документация

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import URLResolver, include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API Documentation for Electronics distribution network",
+        contact=openapi.Contact(email="maks_lakovich@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,6 +26,8 @@ urlpatterns = [
     path("api/", include("users.urls", namespace="users")),
     path("api/", include("products.urls", namespace="products")),
     path("api/", include("network.urls", namespace="network")),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 if settings.DEBUG:
